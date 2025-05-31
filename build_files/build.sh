204466ab23e82831f31910df0257aa5db2d1126b
oleg <jilin.oleg@gmail.com>
2025-05-31 20:26:35 +0300

    add xwayland-sattellite package because I forgot

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,7 +15,7 @@ chmod +x /usr/libexec/aurora-dx-groups
 dnf5 -y copr enable ulysg/xwayland-satellite
 dnf5 -y copr enable yalter/niri
 
-dnf5 install -y blueman brightnessctl foot mako network-manager-applet niri pavucontrol swaybg swaylock waybar wlogout wf-recorder squeekboard
+dnf5 install -y blueman brightnessctl foot mako network-manager-applet niri pavucontrol swaybg swaylock waybar wlogout wf-recorder squeekboard xwayland-satellite
 
 dnf5 -y copr disable ulysg/xwayland-satellite
 dnf5 -y copr disable yalter/niri
