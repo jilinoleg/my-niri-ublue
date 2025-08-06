e9d46b67a98fbeb38f73ee2a7c01460ff8b9d544
oleg <jilin.oleg@gmail.com>
2025-08-06 11:56:55 +0300

    embarrasingly, fix ln arguments being in wrong order

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,7 +34,7 @@ echo ":Installing Doas instead of sudo"
 dnf5 install -y opendoas
 dnf5 remove -y sudo --setopt protected_packages= # remove despite being protected
 
-ln -s /usr/bin/sudo /usr/bin/doas
+ln -s /usr/bin/doas /usr/bin/sudo
 
 
 # Stuff from DX version but without VSCode and Cockpit
