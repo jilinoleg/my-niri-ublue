0665131e8b1a374480d6b01fa0654433d28c3f97
oleg <jilin.oleg@gmail.com>
2025-08-06 11:52:25 +0300

    Another attempt at removing, using setopt argument instead of yum-specific one

--- build_files/build.sh
+++ build_files/build.sh
@@ -32,7 +32,7 @@ dnf5 -y copr disable yalter/niri
 echo ":Installing Doas instead of sudo"
 
 dnf5 install -y opendoas
-dnf5 remove -y sudo --allowerasing # remove despite being protected
+dnf5 remove -y sudo --setopt protected_packages= # remove despite being protected
 
 ln -s /usr/bin/sudo /usr/bin/doas
 
