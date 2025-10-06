2967ba73140f0435558fd5171bca832cc9d20c08
oleg <jilin.oleg@gmail.com>
2025-10-06 17:04:58 +0300

    set suid for Throne core

--- build_files/04_add_proxy.sh
+++ build_files/04_add_proxy.sh
@@ -11,3 +11,9 @@ curl -o /etc/yum.repos.d/throne.repo https://parhelia512.github.io/throne.repo
 dnf5 install -y throne --refresh
 
 rm /etc/yum.repos.d/throne.repo
+
+echo ":Trying to set suid for Throne"
+
+chown root:root
+chmod u+s /usr/lib64/Throne/Core
+
