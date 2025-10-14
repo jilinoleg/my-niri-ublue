40995c422bbdb9edcb46f720e4f5f649e0454a5d
oleg <jilin.oleg@gmail.com>
2025-10-14 19:00:51 +0300

    Stop removing sudo because it removes plasma packages

--- build_files/02_replace_sudo.sh
+++ build_files/02_replace_sudo.sh
@@ -7,6 +7,8 @@ set -ouex pipefail
 echo ":Installing Doas instead of sudo"
 
 dnf5 install -y opendoas
-dnf5 remove -y sudo --setopt protected_packages= # remove despite being protected
 
-ln -s /usr/bin/doas /usr/bin/sudo
+echo ":Not removing sudo because it also removes plasma"
+# dnf5 remove -y sudo --setopt protected_packages= # remove despite being protected
+
+# ln -s /usr/bin/doas /usr/bin/sudo
