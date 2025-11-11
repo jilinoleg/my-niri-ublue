7cc9242ff4678185c660c8bfaabc33f2f59f75f4
oleg <jilin.oleg@gmail.com>
2025-11-11 15:57:24 +0300

    Add DMS shell

--- build_files/03_add_niri.sh
+++ build_files/03_add_niri.sh
@@ -4,12 +4,17 @@ set -ouex pipefail
 
 # Packages for my Niri setup
 
-echo ":Installing Niri and related software"
+echo ":Installing Niri, Dank Material Shell and related software"
 
 dnf5 -y copr enable ulysg/xwayland-satellite
 dnf5 -y copr enable yalter/niri
+dnf5 -y copr enable avengemedia/dms
 
+# TODO - remove software that can be replaced by DMS.
 dnf5 install -y blueman brightnessctl foot mako network-manager-applet niri pavucontrol swaybg swaylock waybar wlogout wf-recorder squeekboard xwayland-satellite
+dnf5 install -y dms
 
 dnf5 -y copr disable ulysg/xwayland-satellite
 dnf5 -y copr disable yalter/niri
+dnf5 -y copr disable avengemedia/dms
+
