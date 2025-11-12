1ea0fc406059ab5d7ff9679a79e0cbfdad5f8f0f
oleg <jilin.oleg@gmail.com>
2025-11-12 12:15:41 +0300

    remove what DMS replaced

--- build_files/03_add_niri.sh
+++ build_files/03_add_niri.sh
@@ -10,9 +10,7 @@ dnf5 -y copr enable ulysg/xwayland-satellite
 dnf5 -y copr enable yalter/niri
 dnf5 -y copr enable avengemedia/dms
 
-# TODO - remove software that can be replaced by DMS.
-dnf5 install -y blueman brightnessctl foot mako network-manager-applet niri pavucontrol swaybg swaylock waybar wlogout wf-recorder squeekboard xwayland-satellite
-dnf5 install -y dms
+dnf5 install -y niri dms brightnessctl foot pavucontrol wf-recorder squeekboard xwayland-satellite
 
 dnf5 -y copr disable ulysg/xwayland-satellite
 dnf5 -y copr disable yalter/niri
