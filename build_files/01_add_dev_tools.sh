59faeb7bf29da01445dd11da668c95a61276bfd3
oleg <jilin.oleg@gmail.com>
2025-11-22 00:56:55 +0300

    remove mozilla-fira-mono-fonts because not in F43

--- build_files/01_add_dev_tools.sh
+++ build_files/01_add_dev_tools.sh
@@ -24,7 +24,7 @@ dnf5 -y copr enable ublue-os/packages
 #podman-bootc
 dnf5 -y copr enable gmaglione/podman-bootc
 
-dnf5 install -y android-tools libvirt libvirt-nss mozilla-fira-mono-fonts osbuild-selinux p7zip p7zip-plugins podman-bootc podman-compose podman-machine podman-tui podmansh powerline-fonts qemu qemu-char-spice  qemu-device-display-virtio-gpu qemu-device-display-virtio-vga qemu-device-usb-redirect qemu-img qemu-system-x86-core qemu-user-binfmt qemu-user-static ublue-os-libvirt-workarounds virt-manager ydotool virt-viewer virt-v2v google-droid-sans-mono-fonts google-go-mono-fonts ibm-plex-mono-fonts intel-one-mono-fonts genisoimage flatpak-builder
+dnf5 install -y android-tools libvirt libvirt-nss  osbuild-selinux p7zip p7zip-plugins podman-bootc podman-compose podman-machine podman-tui podmansh powerline-fonts qemu qemu-char-spice  qemu-device-display-virtio-gpu qemu-device-display-virtio-vga qemu-device-usb-redirect qemu-img qemu-system-x86-core qemu-user-binfmt qemu-user-static ublue-os-libvirt-workarounds virt-manager ydotool virt-viewer virt-v2v google-droid-sans-mono-fonts google-go-mono-fonts ibm-plex-mono-fonts intel-one-mono-fonts genisoimage flatpak-builder
 
 systemctl enable podman.socket
 systemctl enable ublue-os-libvirt-workarounds.service
