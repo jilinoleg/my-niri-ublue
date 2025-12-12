defd007cc33aebd2c7be6c20bf2178435f00b1d7
Oleg <jilin.oleg@gmail.com>
2025-12-12 23:09:48 +0300

    add forgotten -y argument to mako uninstall

--- build_files/03_add_niri.sh
+++ build_files/03_add_niri.sh
@@ -12,7 +12,7 @@ dnf5 -y copr enable avengemedia/dms
 
 dnf5 install -y niri dms brightnessctl foot pavucontrol wf-recorder xwayland-satellite qt6ct adw-gtk3-theme
 
-dnf5 remove mako # explicitly remove mako because something installs it as dependency
+dnf5 remove mako -y # explicitly remove mako because something installs it as dependency
 
 dnf5 -y copr disable ulysg/xwayland-satellite
 dnf5 -y copr disable yalter/niri
